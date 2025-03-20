f5ee9e7c522755800dd2841ac03ad825b381b07f
Gaufre <191967922+gaufre2@users.noreply.github.com>
2025-03-20 12:39:43 +0100

    docs(tmdb): add methods descriptions

--- src/tmdb/tmdb.service.ts
+++ src/tmdb/tmdb.service.ts
@@ -28,6 +28,15 @@ export class TmdbService {
   };
   private CACHE_TTL_SEC = 900; // 15 min
 
+  /**
+   * Searches for a movie using the provided query, language, and optional year.
+   *
+   * @param query - The search query string for the movie.
+   * @param language - The language for the search results (default is 'fr-FR').
+   * @param year - (Optional) The release year of the movie to narrow the search.
+   * @returns A promise that resolves to a `SearchMovieResponseDto` containing the search results.
+   * @throws `InternalServerErrorException` if an error occurs while fetching movie data.
+   */
   private async searchMovie(
     query: string,
     language: string = 'fr-FR',
@@ -58,11 +67,27 @@ export class TmdbService {
     }
   }
 
+  /**
+   * Generates a unique cache key for a given endpoint and its parameters.
+   *
+   * @param endpoint - The API endpoint for which the cache key is being generated.
+   * @param params - An array of strings representing the parameters to include in the cache key.
+   * @returns A string representing the unique cache key, composed of the service name, endpoint, and parameters.
+   */
   private getCacheKey(endpoint: string, params: string[]): string {
     const prefixKey = TmdbService.name.toLowerCase() + `:${endpoint}`;
     const paramsKey = params.map((param) => param).join(':');
     return prefixKey + ':' + paramsKey;
   }
+
+  /**
+   * Retrieves the ID of a movie based on a search query and an optional release year.
+   *
+   * @param query - The search query string for the movie title.
+   * @param year - (Optional) The release year of the movie to narrow down the search.
+   * @returns A promise that resolves to the movie ID if found.
+   * @throws {NotFoundException} If no movie matching the query and optional year is found.
+   */
   async getMovieId(query: string, year?: number): Promise<number> {
     const movieId = await this.searchMovie(query, undefined, year);
 
@@ -75,6 +100,19 @@ export class TmdbService {
     );
   }
 
+  /**
+   * Fetches detailed movie data from the TMDB API, with optional caching.
+   *
+   * @param movieId - The unique identifier of the movie to fetch.
+   * @param ignoreCache - Optional flag to bypass the cache and fetch fresh data. Defaults to `false`.
+   * @param language - The language code for the movie data. Defaults to `'fr-FR'`.
+   * @returns A promise that resolves to a `MovieDetailsResponseDto` containing the movie details.
+   * @throws `InternalServerErrorException` if an error occurs while fetching the movie data.
+   *
+   * This method checks the cache for existing data using a generated cache key.
+   * If the data is not found in the cache or `ignoreCache` is set to `true`, it fetches the data
+   * from the TMDB API. The fetched data is then cached for future requests.
+   */
   async getMovieData(
     movieId: number,
     ignoreCache?: boolean,

717779efdb557bd5f12acdfb3127a66677303a21
Gaufre <191967922+gaufre2@users.noreply.github.com>
2025-04-04 11:27:44 +0200

    feat(tmdb): force Int type for number args

--- src/tmdb/tmdb.resolver.ts
+++ src/tmdb/tmdb.resolver.ts
@@ -1,21 +1,21 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { TmdbService } from './tmdb.service';
 
 @Resolver()
 export class TmdbResolver {
   constructor(private readonly tmdbService: TmdbService) {}
 
-  @Query(() => Number)
+  @Query(() => Int)
   async movieId(
     @Args('query') query: string,
-    @Args('year', { nullable: true }) year?: number,
+    @Args('year', { type: () => Int, nullable: true }) year?: number,
   ) {
     return await this.tmdbService.getMovieId(query, year);
   } // TODO remove, only for test
 
   @Query(() => String)
   async movieData(
-    @Args('movieId') movieId: number,
+    @Args('movieId', { type: () => Int }) movieId: number,
     @Args('ignoreCache') ignoreCache: boolean,
     @Args('language', { nullable: true }) language?: string,
   ) {

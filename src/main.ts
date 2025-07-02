4ffd1a9e54eee060fd805923ccfffe49dfe7d24c
Gaufre <191967922+gaufre2@users.noreply.github.com>
2025-07-02 10:48:04 +0200

    feat(logging): add configurable log levels for the application

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
+import { ConsoleLogger, LogLevel, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new ConsoleLogger({
       prefix: 'NanarDB',
+      logLevels: (process.env.LOGGER_LEVEL?.split(',') as LogLevel[]) ?? [
+        'log',
+        'warn',
+        'error',
+        'fatal',
+      ],
     }),
   });
   app.useGlobalPipes(
